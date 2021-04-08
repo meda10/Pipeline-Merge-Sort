e15cea809eb8dd752ae2abad80580b2357c05b65
meda10 <32466683+meda10@users.noreply.github.com>
2021-04-08 17:46:07 +0200

    test.sh fix

--- test.sh
+++ test.sh
@@ -8,9 +8,8 @@ fi;
 
 number_of_processors=`echo "(l($numbers)/l(2)) + 1" | bc -l | awk '{print int($1)}'`
 
-
-mpic++ -Wall -std=c++11 -pedantic -O3 --prefix /usr/local/share/OpenMPI pms.cpp -o pms
-dd if=/dev/urandom bs=1 count="$numbers" of=numbers >/dev/null 2>&1
+mpic++ -std=c++11 --prefix /usr/local/share/OpenMPI pms.cpp -o pms
+dd if=/dev/urandom bs=1 count=$numbers of=numbers >/dev/null 2>&1
 echo $number_of_processors
 mpirun --prefix /usr/local/share/OpenMPI -np $number_of_processors pms
 rm -f pms numbers
